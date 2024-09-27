66cce10607a2e6b20916a3ed6d35afaaa11d32fc
grace <gracemwangi84@gmail.com>
2024-09-28 00:54:01 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2 ;
+
+const random = Math.floor(Math.random()* 1) + 1 ;
+
+const num3= 24
+const num4= 5
+const mod = num3%num4;
+
+
+const num5=[20,5,3,12];
+const max = Math.max(...num5);
+
+
